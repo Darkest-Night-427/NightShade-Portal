fb940d35e1f53888d9cbea61c5538c9b5c43f23b
Darkest-Night-427 <darkestnight427@gmail.com>
2026-09-06 16:40:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const searchForm = document.getElementById("searchForm");
+const searchBox = document.getElementById("searchBox");
 
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const query = searchBox.value.trim();
+
+    if (query === "") return;
+
+    const url = "https://www.google.com/search?q=" + encodeURIComponent(query);
+
+    window.open(url, "_blank");
+});
